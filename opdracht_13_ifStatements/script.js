869111301764aa981dd1f93b3f342788f929ab38
DashaDafusun <84176953+DashaDafusun@users.noreply.github.com>
2021-10-19 13:58:40 +0200

    Add files via upload

---
+++ opdracht_13_ifStatements/script.js
@@ -0,0 +1,53 @@
+
+// Deel 1
+
+const age = 17;
+
+if (age < 18) {
+
+    console.log("Sorry, jij bent te jong...");
+
+} else if (age === 17) {
+
+    console.log("Sorry, wij zien jou volgend jaar!");
+
+} else if (age >= 18) {
+
+    console.log("Hartelijk Welkom!");
+
+} if (age > 70) {
+
+    console.log("Jij krijgt ons respect en een gratis pils!");
+};
+
+
+
+
+
+// Deel 2
+
+const isFemale = false;
+
+if (isFemale) {
+
+    console.log('Oh yes! Let us get started!');
+} else { 
+
+    console.log('Ooeps... It is only for ladies!');
+};
+
+
+
+
+// Deel 3 
+
+const driverStatus = 'bob';
+
+if (driverStatus === 'bob') {
+
+    console.log('Ben jij Bob? Zeg het hardop! En jij mag driven!');
+
+} else {
+
+    console.log('Glasje op? Lekker (en veilig) met de bus naar huis!');
+};
